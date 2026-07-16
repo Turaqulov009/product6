3496e7b82a1603b94d729724d3bff7142a994bca
Turaqulov009 <aziztrqlv@gmail.com>
2026-07-16 10:20:03 +0500

    update

--- product6/urls.py
+++ product6/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import ProductViewSet
+from .views import ProductViewSet, ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, ProductUpdateAPIView, ProductDestroyAPIView, ProductRetrieveUpdateAPIView, ProductListCreateAPIView, ProductRetrieveDestroyAPIView, ProductRetrieveUpdateDestroyAPIView
 
-urlpatterns = [
-    path("", ProductViewSet.as_view({"post": "create"}))
+urlpatterns=[
+    path('', ProductViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('product6/list/', ProductListAPIView.as_view()),
+    path('product6/create/', ProductCreateAPIView.as_view()),
+    path('product6/retrieve/<int:pk>/', ProductRetrieveAPIView.as_view()),
+    path('product6/update/<int:pk>/', ProductUpdateAPIView.as_view()),
+    path('product6/destroy/<int:pk>/', ProductDestroyAPIView.as_view()),
+    path('product6/retrieve-update/<int:pk>/', ProductRetrieveUpdateAPIView.as_view()),
+    path('product6/list-create/', ProductListCreateAPIView.as_view()),
+    path('product6/retrieve-destroy/<int:pk>/', ProductRetrieveDestroyAPIView.as_view()),
+    path('product6/retrieve-update-destroy/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view()),
 ]
--- product6/views.py
+++ product6/views.py
@@ -1,9 +1,53 @@
+
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, RetrieveUpdateAPIView, ListCreateAPIView, RetrieveDestroyAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductCreateAPIView(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductRetrieveAPIView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductUpdateAPIView(UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDestroyAPIView(DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductListCreateAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductRetrieveDestroyAPIView(RetrieveDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
